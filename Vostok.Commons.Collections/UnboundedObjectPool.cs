db807073516a60dc17ac26cf1775d2e223544af7
iloktionov <iloktionov@skbkontur.ru>
2018-08-05 19:39:36 +0500

    Added a very simple unbounded object pool.

---
+++ Vostok.Commons.Collections/UnboundedObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Collections
+{
+    [PublicAPI]
+#if MAKE_CLASSES_PUBLIC
+    public
+#else
+    internal
+#endif
+    class UnboundedObjectPool<T>
+    {
+        private readonly ConcurrentQueue<T> items = new ConcurrentQueue<T>();
+        private readonly Func<T> itemFactory;
+
+        public UnboundedObjectPool(Func<T> itemFactory)
+        {
+            this.itemFactory = itemFactory;
+        }
+
+        public T Acquire()
+        {
+            return items.TryDequeue(out var item) ? item : itemFactory();
+        }
+
+        public void Return(T item)
+        {
+            items.Enqueue(item);
+        }
+    }
+}

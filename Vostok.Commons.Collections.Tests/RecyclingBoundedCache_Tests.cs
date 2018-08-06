2ce3009a756703c6ac8f870ddff80e399cb48df0
iloktionov <iloktionov@skbkontur.ru>
2018-08-06 17:21:22 +0500

    + RecyclingBoundedCache

---
+++ Vostok.Commons.Collections.Tests/RecyclingBoundedCache_Tests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Collections.Tests
+{
+    [TestFixture]
+    internal class RecyclingBoundedCache_Tests
+    {
+        private RecyclingBoundedCache<string, int> cache;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            cache = new RecyclingBoundedCache<string, int>(3);
+        }
+
+        [Test]
+        public void Obtain_should_cache_first_observed_value_and_serve_it_on_later_invocations()
+        {
+            cache.Obtain("key", _ => 1).Should().Be(1);
+            cache.Obtain("key", _ => 2).Should().Be(1);
+            cache.Obtain("key", _ => 3).Should().Be(1);
+        }
+
+        [Test]
+        public void Obtain_should_not_clean_cache_until_capacity_is_exceeded()
+        {
+            cache.Obtain("key1", _ => 1).Should().Be(1);
+            cache.Obtain("key2", _ => 1).Should().Be(1);
+            cache.Obtain("key3", _ => 1).Should().Be(1);
+
+            cache.Obtain("key1", _ => 2).Should().Be(1);
+            cache.Obtain("key2", _ => 2).Should().Be(1);
+            cache.Obtain("key3", _ => 2).Should().Be(1);
+        }
+
+        [Test]
+        public void Obtain_should_wipe_the_cache_when_capacity_is_exceeded()
+        {
+            cache.Obtain("key1", _ => 1).Should().Be(1);
+            cache.Obtain("key2", _ => 1).Should().Be(1);
+            cache.Obtain("key3", _ => 1).Should().Be(1);
+            cache.Obtain("key4", _ => 1).Should().Be(1);
+
+            cache.Obtain("key1", _ => 2).Should().Be(2);
+            cache.Obtain("key2", _ => 2).Should().Be(2);
+            cache.Obtain("key3", _ => 2).Should().Be(2);
+        }
+    }
+}
---
+++ Vostok.Commons.Collections/RecyclingBoundedCache.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Collections
+{
+    [PublicAPI]
+#if MAKE_CLASSES_PUBLIC
+    public
+#else
+    internal
+#endif
+    class RecyclingBoundedCache<TKey, TValue>
+    {
+        private readonly int capacity;
+        private readonly IEqualityComparer<TKey> comparer;
+        private RecyclingBoundedCacheState state;
+
+        public RecyclingBoundedCache(int capacity, [CanBeNull] IEqualityComparer<TKey> comparer = null)
+        {
+            this.capacity = capacity;
+            this.comparer = comparer ?? EqualityComparer<TKey>.Default;
+
+            state = new RecyclingBoundedCacheState(this.comparer);
+        }
+
+        public TValue Obtain(TKey key, Func<TKey, TValue> factory)
+        {
+            var currentState = state;
+            if (currentState.Items.TryGetValue(key, out var value))
+                return value;
+
+            if (currentState.Items.TryAdd(key, value = factory(key)))
+            {
+                var newCount = Interlocked.Increment(ref currentState.Count);
+                if (newCount > capacity)
+                    Interlocked.Exchange(ref state, new RecyclingBoundedCacheState(comparer));
+            }
+
+            return value;
+        }
+
+        private class RecyclingBoundedCacheState
+        {
+            public readonly ConcurrentDictionary<TKey, TValue> Items;
+
+            public int Count;
+
+            public RecyclingBoundedCacheState(IEqualityComparer<TKey> comparer)
+            {
+                Items = new ConcurrentDictionary<TKey, TValue>(comparer);
+            }
+        }
+    }
+}

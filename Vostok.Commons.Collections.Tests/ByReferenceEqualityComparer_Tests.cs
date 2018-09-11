f125ab608369f90768cd8507b2ba87e230d1ac7f
iloktionov <iloktionov@skbkontur.ru>
2018-09-11 17:57:56 +0500

    + ByReferenceEqualityComparer, CachingTransform

---
+++ Vostok.Commons.Collections.Tests/ByReferenceEqualityComparer_Tests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Collections.Tests
+{
+    [TestFixture]
+    internal class ByReferenceEqualityComparer_Tests
+    {
+        private ByReferenceEqualityComparer<string> comparer;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            comparer = new ByReferenceEqualityComparer<string>();
+        }
+
+        [Test]
+        public void Equals_should_return_true_for_references_pointing_to_same_object()
+        {
+            var value = "value";
+            var value1 = value;
+            var value2 = value;
+
+            comparer.Equals(value1, value2).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equals_should_return_false_for_equal_but_not_same_objects()
+        {
+            var value = "value";
+            var value1 = new string(value.ToCharArray());
+            var value2 = new string(value.ToCharArray());
+
+            comparer.Equals(value1, value2).Should().BeFalse();
+        }
+
+        [Test]
+        public void GetHashCode_should_return_same_values_for_references_pointing_to_same_object()
+        {
+            var value = "value";
+            var value1 = value;
+            var value2 = value;
+
+            comparer.GetHashCode(value1).Should().Be(comparer.GetHashCode(value2));
+        }
+
+        [Test]
+        public void GetHashCode_should_return_different_values_for_equal_but_not_same_objects()
+        {
+            var value = "value";
+            var value1 = new string(value.ToCharArray());
+            var value2 = new string(value.ToCharArray());
+
+            comparer.GetHashCode(value1).Should().NotBe(comparer.GetHashCode(value2));
+        }
+    }
+}
---
+++ Vostok.Commons.Collections.Tests/CachingTransform_Tests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Collections.Tests
+{
+    [TestFixture(true)]
+    [TestFixture(false)]
+    internal class CachingTransform_Tests
+    {
+        private readonly bool preventParallelProcessing;
+
+        private CachingTransform<string, string> transform;
+
+        private string providedValue;
+
+        private int processorCalls;
+
+        public CachingTransform_Tests(bool preventParallelProcessing)
+        {
+            this.preventParallelProcessing = preventParallelProcessing;
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            providedValue = "xxx";
+
+            processorCalls = 0;
+
+            transform = new CachingTransform<string, string>(i =>
+            {
+                processorCalls++;
+                return i.ToUpper();
+            }, () => providedValue, preventParallelProcessing: preventParallelProcessing);
+        }
+
+        [Test]
+        public void Get_without_parameters_should_work_when_there_is_no_cache()
+        {
+            transform.Get().Should().Be("XXX");
+        }
+
+        [Test]
+        public void Get_without_parameters_should_return_cached_value_without_processing_when_raw_value_does_not_change()
+        {
+            var result1 = transform.Get();
+            var result2 = transform.Get();
+            var result3 = transform.Get();
+
+            result2.Should().BeSameAs(result1);
+            result3.Should().BeSameAs(result1);
+
+            processorCalls.Should().Be(1);
+        }
+
+        [Test]
+        public void Get_without_parameters_should_react_to_raw_value_change()
+        {
+            transform.Get().Should().Be("XXX");
+
+            providedValue = "xxxx";
+
+            transform.Get().Should().Be("XXXX");
+        }
+
+        [Test]
+        public void Get_with_parameter_should_always_return_correct_transformed_value()
+        {
+            providedValue = "x";
+
+            transform.Get("xx").Should().Be("XX");
+            transform.Get();
+            transform.Get("xxx").Should().Be("XXX");
+            transform.Get();
+            transform.Get("xxxx").Should().Be("XXXX");
+        }
+    }
+}

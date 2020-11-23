7eeae35bd68c2d896038df7dce7c09947ea4ce72
iloktionov <iloktionov@skbkontur.ru>
2020-11-23 16:34:57 +0500

    Implemented a simple LRU cache.

---
+++ Vostok.Commons.Collections.Tests/LRUCache_Tests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Collections.Tests
+{
+    [TestFixture]
+    internal class LRUCache_Tests
+    {
+        private LRUCache<string, int> cache;
+
+        [SetUp]
+        public void TestSetup()
+            => cache = new LRUCache<string, int>(3);
+
+        [Test]
+        public void GetOrAdd_should_return_cached_values()
+        {
+            cache.GetOrAdd("key", _ => 1).Should().Be(1);
+            cache.GetOrAdd("key", _ => 2).Should().Be(1);
+            cache.GetOrAdd("key", _ => 3).Should().Be(1);
+        }
+
+        [Test]
+        public void GetOrAdd_should_evict_least_recently_used()
+        {
+            cache.GetOrAdd("key1", _ => 1).Should().Be(1);
+            cache.GetOrAdd("key2", _ => 2).Should().Be(2);
+            cache.GetOrAdd("key3", _ => 3).Should().Be(3);
+
+            cache.TryGet("key2", out _).Should().BeTrue();
+            cache.TryGet("key1", out _).Should().BeTrue();
+            cache.TryGet("key3", out _).Should().BeTrue();
+
+            cache.GetOrAdd("key3", _ => 4).Should().Be(3);
+            cache.GetOrAdd("key4", _ => 5).Should().Be(5);
+            cache.GetOrAdd("key5", _ => 6).Should().Be(6);
+
+            cache.Select(pair => pair.Key).Should().BeEquivalentTo("key3", "key4", "key5");
+        }
+    }
+}
---
+++ Vostok.Commons.Collections/LRUCache.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Collections
+{
+    [PublicAPI]
+    internal class LRUCache<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private readonly int capacity;
+        private readonly IEqualityComparer<TKey> comparer;
+
+        private readonly LinkedList<(TKey key, TValue value)> queue;
+        private readonly ConcurrentDictionary<TKey, LinkedListNode<(TKey key, TValue value)>> map;
+
+        public LRUCache(int capacity, [CanBeNull] IEqualityComparer<TKey> comparer = null)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), "Cache capacity must be positive");
+
+            this.capacity = capacity;
+            this.comparer = comparer ?? EqualityComparer<TKey>.Default;
+
+            queue = new LinkedList<(TKey, TValue)>();
+            map = new ConcurrentDictionary<TKey, LinkedListNode<(TKey, TValue)>>(this.comparer);
+        }
+
+        public int Count => map.Count;
+
+        public bool TryGet(TKey key, out TValue value)
+        {
+            if (!map.TryGetValue(key, out var node))
+            {
+                value = default;
+                return false;
+            }
+
+            value = node.Value.value;
+
+            RepositionNode();
+
+            return true;
+
+            void RepositionNode()
+            {
+                if (node.List == null)
+                    return;
+
+                lock (queue)
+                {
+                    if (node.List == null)
+                        return;
+
+                    queue.Remove(node);
+                    queue.AddLast(node);
+                }
+            }
+        }
+
+        public TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
+            => GetOrAddAsync(key, k => Task.FromResult(valueFactory(k))).GetAwaiter().GetResult();
+
+        public async Task<TValue> GetOrAddAsync(TKey key, Func<TKey, Task<TValue>> valueFactory)
+        {
+            while (true)
+            {
+                if (TryGet(key, out var value))
+                    return value;
+
+                var node = new LinkedListNode<(TKey key, TValue value)>((key, await valueFactory(key)));
+
+                if (!map.TryAdd(key, node))
+                    continue;
+
+                LinkedListNode<(TKey key, TValue value)> first = null;
+
+                lock (queue)
+                {
+                    if (queue.Count >= capacity)
+                    {
+                        first = queue.First;
+                        queue.RemoveFirst();
+                    }
+
+                    queue.AddLast(node);
+                }
+
+                if (first != null)
+                    map.TryRemove(first.Value.key, out _);
+
+                return node.Value.value;
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            if (!map.TryRemove(key, out var node))
+                return;
+
+            lock (queue)
+            {
+                if (node.List == null)
+                    return;
+
+                queue.Remove(node);
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => 
+            map.Select(pair => new KeyValuePair<TKey, TValue>(pair.Value.Value.key, pair.Value.Value.value)).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}

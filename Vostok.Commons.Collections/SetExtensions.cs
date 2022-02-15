edb201e67a001e694a8ed7901de9f82e5ff04a7c
a.tolstov <a.tolstov@skbkontur.ru>
2022-02-15 07:06:15 +0500

    fix: small build problems

--- Vostok.Commons.Collections/SetExtensions.cs
+++ Vostok.Commons.Collections/SetExtensions.cs
@@ -19,4 +19,41 @@ namespace Vostok.Commons.Collections
             return new(inLeft, same, inRight);
         }
     }
+    
+    internal class SetDiff<T>
+    {
+#if !NETSTANDARD2_0
+        public IReadOnlySet<T> OnlyInLeft { get; }
+        public IReadOnlySet<T> Same { get; }
+        public IReadOnlySet<T> OnlyInRight { get; }
+#else
+        public IReadOnlyCollection<T> OnlyInLeft { get; }
+        public IReadOnlyCollection<T> Same { get; }
+        public IReadOnlyCollection<T> OnlyInRight { get; }
+#endif
+
+        public IEnumerable<T> Different => OnlyInLeft.Concat(OnlyInRight);
+
+#if !NETSTANDARD2_0
+        public SetDiff(
+            IReadOnlySet<T> onlyInLeft,
+            IReadOnlySet<T> same,
+            IReadOnlySet<T> onlyInRight)
+        {
+            OnlyInLeft = onlyInLeft;
+            Same = same;
+            OnlyInRight = onlyInRight;
+        }
+#else
+        public SetDiff(
+            IReadOnlyCollection<T> onlyInLeft,
+            IReadOnlyCollection<T> same,
+            IReadOnlyCollection<T> onlyInRight)
+        {
+            OnlyInLeft = onlyInLeft;
+            Same = same;
+            OnlyInRight = onlyInRight;
+        }
+#endif
+    }
 }

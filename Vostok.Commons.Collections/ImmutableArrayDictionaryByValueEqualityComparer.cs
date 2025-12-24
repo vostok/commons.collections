ebde0490db05815496bdc6dced02e1c1a339e5b8
d.khrustalev <d.khrustalev@skbkontur.ru>
2025-12-24 12:32:43 +0500

    block-scoped namespace

--- Vostok.Commons.Collections/ImmutableArrayDictionaryByValueEqualityComparer.cs
+++ Vostok.Commons.Collections/ImmutableArrayDictionaryByValueEqualityComparer.cs
@@ -3,54 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 
-namespace Vostok.Commons.Collections;
-
-[PublicAPI]
-internal class ImmutableArrayDictionaryByValueEqualityComparer<TKey, TValue> : IEqualityComparer<ImmutableArrayDictionary<TKey, TValue>>
+namespace Vostok.Commons.Collections
 {
-    public static readonly ImmutableArrayDictionaryByValueEqualityComparer<TKey, TValue> Instance = new();
-
-    public bool Equals(ImmutableArrayDictionary<TKey, TValue> x, ImmutableArrayDictionary<TKey, TValue> y)
+    [PublicAPI]
+    internal class ImmutableArrayDictionaryByValueEqualityComparer<TKey, TValue> : IEqualityComparer<ImmutableArrayDictionary<TKey, TValue>>
     {
-        if (ReferenceEquals(x, y))
-            return true;
-        if (x is null || y is null)
-            return false;
-        if (x.GetType() != y.GetType())
-            return false;
-        if (x.Count != y.Count)
-            return false;
+        public static readonly ImmutableArrayDictionaryByValueEqualityComparer<TKey, TValue> Instance = new();
 
-        foreach (var pair in x)
+        public bool Equals(ImmutableArrayDictionary<TKey, TValue> x, ImmutableArrayDictionary<TKey, TValue> y)
         {
-            if (!y.TryGetValue(pair.Key, out var yValue)) return false;
-            if (!EqualityComparer<TValue>.Default.Equals(pair.Value, yValue)) return false;
-        }
+            if (ReferenceEquals(x, y))
+                return true;
+            if (x is null || y is null)
+                return false;
+            if (x.GetType() != y.GetType())
+                return false;
+            if (x.Count != y.Count)
+                return false;
+
+            foreach (var pair in x)
+            {
+                if (!y.TryGetValue(pair.Key, out var yValue)) return false;
+                if (!EqualityComparer<TValue>.Default.Equals(pair.Value, yValue)) return false;
+            }
         
-        return true;
-    }
-
-    public int GetHashCode(ImmutableArrayDictionary<TKey, TValue> obj)
-    {
-        var hKeys = 0;
-        var hValues = 0;
-        foreach (var pair in obj.OrderBy(kvp => kvp.Key))
-        {
-            hKeys ^= pair.Key.GetHashCode();
-            hValues ^= pair.Value.GetHashCode();
+            return true;
         }
+
+        public int GetHashCode(ImmutableArrayDictionary<TKey, TValue> obj)
+        {
+            var hKeys = 0;
+            var hValues = 0;
+            foreach (var pair in obj.OrderBy(kvp => kvp.Key))
+            {
+                hKeys ^= pair.Key.GetHashCode();
+                hValues ^= pair.Value.GetHashCode();
+            }
 
 #if NETSTANDARD2_0
-        var hash = 23;
-        hash = unchecked(hash * 31 + hKeys);
-        hash = unchecked(hash * 31 + hValues);
-        return hash;
+            var hash = 23;
+            hash = unchecked(hash * 31 + hKeys);
+            hash = unchecked(hash * 31 + hValues);
+            return hash;
 #else
-        return HashCode.Combine(hKeys, hValues);
+            return HashCode.Combine(hKeys, hValues);
 #endif
-    }
+        }
 
-    private ImmutableArrayDictionaryByValueEqualityComparer()
-    {
+        private ImmutableArrayDictionaryByValueEqualityComparer()
+        {
+        }
     }
 }
